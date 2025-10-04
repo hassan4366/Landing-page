0a55c465f44f6e530f9a88732971305231149a48
Hassan Hammam <hassanibrahimhammam@gmail.com>
2025-10-04 18:03:09 +0300

    Finish-project

---
+++ style-image/main.js
@@ -0,0 +1,16 @@
+let backToTopBtn = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+};
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
